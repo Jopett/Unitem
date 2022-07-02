b9fb553f73f07137b4c109d154b4c11c6a25cbe5
KonkukCodeCat <mr8356@naver.com>
2022-07-02 12:52:02 +0900

    Add module bunjang danggeun jgnara

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BunjangModule } from './bunjang/bunjang.module';
+import { JgnaraModule } from './jgnara/jgnara.module';
+import { DanggeunModule } from './danggeun/danggeun.module';
 
 @Module({
-  imports: [],
+  imports: [BunjangModule, JgnaraModule, DanggeunModule],
   controllers: [AppController],
   providers: [AppService],
 })
